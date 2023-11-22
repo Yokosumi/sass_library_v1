02350ac1f09b6b1b7bbd48479c8a1dafea75d483
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-22 23:34:59 +0100

    folder structure set

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import './main.scss';
 function App() {
-    return <></>
+    return (
+        <>
+            <h1 className="typo-default">Header</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
